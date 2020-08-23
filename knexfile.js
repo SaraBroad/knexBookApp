2f79792926e170a5ff0a0d9b62f69ba672ebd2d3
SaraBroad <saraebroad@gmail.com>
2020-08-22 20:19:55 -0400

    add knex

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// this contains knex queries that can be made without Express
+
+require('dotenv').config()
+const knex = require('./db/knex.js')
+
+function selectAll() {
+    knex.select('*').from('Books')
+    .then(result => {
+        console.log(result)
+    })
+}
+
+// selectAll()
+
+function queryById(id) {
+    knex('Books').where('id', id)
+    .then(result => {
+        console.log('QueryById result: ', result)
+    })
+}
+
+// queryById(4)
+
+
+function ascendingRatingSort() {
+    knex('Books').orderBy('rating', 'asc')
+    .then(result => {
+        console.log(result)
+    })
+}
+
+// ascendingRatingSort()
